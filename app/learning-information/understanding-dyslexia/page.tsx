3f8926b44043917cfe453ebedc839b7459396968
_Kentoyyy <kenalcantara55@gmail.com>
2024-11-06 19:52:14 +0800

    up

--- app/learning-information/understanding-dyslexia/page.tsx
+++ app/learning-information/understanding-dyslexia/page.tsx
@@ -150,7 +150,7 @@ const UnderstandingDyslexia = () => {
                     </p>
                     <ul className="list-disc pl-5 text-gray-600 text-base leading-relaxed mb-6 font-raleway font-medium space-y-2">
                         <li>Find it difficult to decode logos and signs</li>
-                        <li>Lack fluency compared to other children their age</li>
+                        <li>Struggle when trying to learn the rules to games</li>
                         <li>Reverse letters and numbers when reading (read <em>saw</em> as <em>was</em>, for example)</li>
                         <li>Find it difficult to take notes and copy down words from the board</li>
                         <li>Struggle with rhyming, associating sounds with letters, and sequencing and ordering sounds</li>
