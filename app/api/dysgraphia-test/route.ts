2f03fd85a340244dabd86771d62ca9e45369393b
_Kentoyyy <kenalcantara55@gmail.com>
2025-01-14 00:12:13 +0800

    up

---
+++ app/api/dysgraphia-test/route.ts
@@ -0,0 +1,28 @@
+import connect from "@/utils/db";
+import User from "@/models/User";
+import { NextResponse } from "next/server";
+
+export const POST = async (request: Request) => {
+  await connect();
+  const { userId, prediction, confidence, severity } = await request.json();
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    user.testResults.push({
+      testType: "Dysgraphia",
+      result: prediction,
+      severity,
+      confidence,
+    });
+
+    await user.save();
+    return NextResponse.json({ message: "Test result saved successfully" }, { status: 200 });
+  } catch (error) {
+    console.error("Error saving test result:", error);
+    return new NextResponse("Failed to save test result", { status: 500 });
+  }
+};
